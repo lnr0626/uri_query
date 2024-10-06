5061918f53b1447097388698f6e432bca61d4246
Lloyd Ramey <lnr0626@gmail.com>
2024-10-05 22:17:44 -0400

    revert back to 1.15 as min elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule UriQuery.Mixfile do
   def project do
     [app: :uri_query,
      version: "0.2.0",
-     elixir: "~> 1.16",
+     elixir: "~> 1.15",
      build_embedded: Mix.env() == :prod,
      start_permanent: Mix.env() == :prod,
      deps: deps(),
